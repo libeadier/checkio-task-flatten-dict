e5eb7299263707cc89be6677ead571c6abbdc766
Valentin Bryukhanov <bvv.mag@gmail.com>
2014-07-15 12:35:49 +1200

    fix function name in js

--- editor/animation/init.js
+++ editor/animation/init.js
@@ -40,7 +40,7 @@ requirejs(['ext_editor_1', 'jquery_190', 'raphael_210'],
             }
 
             //YOUR FUNCTION NAME
-            var fname = 'checkio';
+            var fname = 'flatten';
 
             var checkioInput = data.in;
             var checkioInputStr = ' ' + fname + '(' + JSON.stringify(checkioInput)  + ')';
